f6310a152544360d7bd62c1104ca2ecfb1d1d4bd
Nadman Alvee <nadmanalvee@gmail.com>
2024-12-18 00:03:55 +0600

    v1.0.4

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
 /*
- * Title: Uptime Monitoring Application
- * Description: A RESTFul API to monitor up or down time for user defined url
+ * Title: initial project file
+ * Description: initial file to start the node server and background process 
  * Author: Nadman Alvee
- * Date: 11/19/2024
+ * Date: 12/17/2024
 */
 
 // dependencies
-const http = require('http');
-const {handleReqRes} = require('./helpers/handleReqRes');
-const environment = require('./helpers/environments');
+const server = require('./lib/server');
+const workers = require('./lib/worker');
 
 // app object - module scaffolding
 const app = {};
 
+app.init = ()=>{
+    // start the server
+    server.init();
+    // start the workers
+    workers.init();
+}
 
-// create server
-app.createServer = function(){
-    const server = http.createServer(app.handleReqRes); // app.handle will handle req res from server
+app.init();
 
-    server.listen(environment.port, ()=>{
-        console.log(`listeing to port ${environment.port}`); // listening to the port of corresponding environment
-    });
-};
-
-// handle req res
-app.handleReqRes = handleReqRes;
-
-// start the server
-app.createServer();
-
-//@ todo
-const { sendTwilioSms } = require('./helpers/notifications');
-sendTwilioSms('01763914143', 'Hello World', (err) => {
-    console.log(err);
-});
+// export the module
+module.exports = app;
